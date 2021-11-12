844bc46022f4fa7e8113572c43668ca9831fd8ca
My Name <jberolm@unex.es>
2021-11-12 17:59:22 +0100

    Shopping Center Mobile app

---
+++ app/src/main/java/com/unexuma/mobile_shoppingcenter/service/MQTTConfiguration.java
@@ -0,0 +1,8 @@
+package com.unexuma.mobile_shoppingcenter.service;
+
+public class MQTTConfiguration {
+
+    //public static String MQTT_BROKER_URL = "tcp://169.254.41.45:1883";
+    //public static String MQTT_BROKER_URL = "tcp://108.129.48.139:1883";
+    public static String MQTT_BROKER_URL = "tcp://broker.hivemq.com:1883";
+}
